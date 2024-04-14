13f6034bff9f1997c4d4b2b583a97b2927050fa9
alexandrovvaleriu <alexandrovvaleriu1@gmail.com>
2024-04-14 10:34:28 +0300

    I have added examples of primitive variables

---
+++ src/main/java/md/tekwillacademy/variablesandcomments/CommentsAndVariables.java
@@ -0,0 +1,27 @@
+package md.tekwillacademy.variablesandcomments;
+
+public class CommentsAndVariables {
+
+    public static void main(String[] args) {
+    boolean isHotOutside;
+    isHotOutside = true;
+        System.out.println(isHotOutside);
+        boolean isSunday = true;
+        System.out.println(isSunday);
+        byte myAge = 28;
+        System.out.println(myAge);
+        char myName = 'V';
+        System.out.println(myName);
+        short myWeight = 65;
+        int myBirtday = 301996;
+        System.out.println(myWeight);
+        System.out.println(myBirtday);
+        long myHeight = 170;
+        float myTeam = 35.45f;
+        double myWork = 956.67454;
+        System.out.println(myHeight);
+        System.out.println(myTeam);
+        System.out.println(myWork);
+}
+
+}
